081df18415dd95c460af6b8f93a431fc91738bd9
Machad3x <machad3x@gmail.com>
2025-04-28 17:16:06 -0400

    dm2q: Update blobs from S916BXXU8DYD9

--- lineage_dm2q.mk
+++ lineage_dm2q.mk
@@ -17,13 +17,13 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 PRODUCT_NAME := lineage_dm2q
 PRODUCT_DEVICE := dm2q
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-S9160
+PRODUCT_MODEL := SM-S916B
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="dm2qzhx-user 15 AP3A.240905.015.A2 S9160ZHU6DYD9 release-keys" \
-    BuildFingerprint=samsung/dm2qzhx/dm2q:15/AP3A.240905.015.A2/S9160ZHU6DYD9:user/release-keys \
-    DeviceProduct=dm2qzhx \
-    SystemName=dm2qzhx
+    BuildDesc="dm2qxxx-user 15 AP3A.240905.015.A2 S916BXXU8DYD9 release-keys" \
+    BuildFingerprint=samsung/dm2qxxx/dm2q:15/AP3A.240905.015.A2/S916BXXU8DYD9:user/release-keys \
+    DeviceProduct=dm2qxxx \
+    SystemName=dm2qxxx
