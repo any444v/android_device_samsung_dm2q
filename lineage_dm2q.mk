781f9b071294d7326a87be56d52c119aa7dc9aac
any444v <162444641+any444v@users.noreply.github.com>
2025-07-02 10:59:33 +0500

    specify some variables

--- lineage_dm2q.mk
+++ lineage_dm2q.mk
@@ -8,17 +8,18 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from the device configuration.
+# Inherit from dm2q device
 $(call inherit-product, device/samsung/dm2q/device.mk)
 
 # Inherit from the Lineage configuration.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_dm2q
 PRODUCT_DEVICE := dm2q
+PRODUCT_MANUFACTURER := samsung
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-S916B
-PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
@@ -27,9 +28,10 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildFingerprint=samsung/dm2qxxx/dm2q:15/AP3A.240905.015.A2/S916BXXS8DYF1:user/release-keys \
     DeviceProduct=dm2qxxx \
     SystemName=dm2qxxx \
-    RisingChipset="Snapdragon 8 Gen 2" \
+    RisingChipset="Snapdragon® 8 Gen 2" \
     RisingMaintainer="3kz"
 
+# RisingOS variables
 RISING_MAINTAINER := 3kz
 
 TARGET_ENABLE_BLUR := true
